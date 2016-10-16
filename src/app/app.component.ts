d76a89a647f5a959ac0055c91d313910e80a23c1
Gabriel Trabanco <gtrabanco@fwok.org>
2016-10-16 04:19:14 +0100

    Added article component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model.ts';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  articles:Article[];
 
+
+  constructor() {
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 10),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular 1', 'http://angular.org')
+    ];
+  }
+
+
+  addArticle(title:HTMLInputElement, link:HTMLInputElement):boolean {
+    console.log("Adding link with title \"%s\" and url: %s", title.value, link.value);
+
+    this.articles.push(new Article(title.value, link.value));
+
+    title.value ='';
+    link.value = '';
+
+    return false;
+  }
 }
